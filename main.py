79e2784819e8dc2ead120f1017a167278d3fbd46
max31ru12 <maxevg72@gmail.com>
2023-01-17 22:04:51 +0300

    Learned how to add keybords and bottons

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 # import asyncio
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
 
 # Base bot's configurations
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -23,10 +25,32 @@ HELP_COMMAND = '''
 /start - начать работу с ботом
 '''
 
+# Создаем клавиатуру
+# resize_keyboard - подстраивает клавиатуру под ТГ более красиво (default - False)
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+# Добавляем в клавиатуру кнопку
+kb.add(KeyboardButton('/help'))
+botton_two = KeyboardButton('/photo')
+botton_three = KeyboardButton('/give')
+# Добавляет новую клавищу (на новую линию)
+kb.add(botton_two)
+# Добавляет новую кнопку (в новый стобец последней строки)
+kb.insert(botton_three)
+
 
 # Функция, которая выполняется, когда включается бот
 async def on_start_up(_):
     print('Бот был успешно запущен')
+
+
+# START-command
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    # Поддерживает какую-то неадекватную HTML-разметку (типа курсив)
+    await message.answer('<em>Привет!</em>',
+                         parse_mode='HTML',
+                         reply_markup=kb)
+# reply_markup - сюда передается клавиатура, которую нужно создать заранее
 
 
 @dp.message_handler(commands=['echo'])
@@ -50,15 +74,11 @@ async def send_image(message: types.Message):
 # Обрабатываем команду \help
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await message.reply(HELP_COMMAND)
+    await message.reply(HELP_COMMAND,
+                        reply_markup=ReplyKeyboardRemove())
+    # ReplyKeyboardRemove - удаляет клавиатуру по этой команде
     # # Delete last message
     # await message.delete()
-
-
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    # Поддерживает какую-то неадекватную HTML-разметку (типа курсив)
-    await message.answer('<em>Привет!</em>', parse_mode='HTML')
 
 
 @dp.message_handler(commands=['give'])
@@ -69,4 +89,6 @@ async def sticker_message(message: types.Message):
 
 if __name__ == '__main__':
     # Запускаем бота в режиме поллинга
+    # skip_updates = False (так по умолчанию)- юзер пиишет боту, бот не онлайн, когла
+    # бот заходит, то отвечает на все сообщения
     executor.start_polling(dp, skip_updates=True, on_startup=on_start_up)
